28401698728eab7fa22d8156ca042bfd2b5b0d0c
Bobby Towers <porkostomus@mail.com>
2022-10-19 16:29:59 -0700

    use modified local analyzer

---
+++ src/clojure_representer.clj
@@ -0,0 +1,27 @@
+(ns clojure-representer
+  (:require [analyzer.jvm :as ana.jvm]
+            [analyzer.passes.jvm.emit-form :as e]
+            [analyzer.passes.uniquify :refer [mappings placeholder]]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [cheshire.core :as json]
+            [clojure.pprint :as pp]))
+
+(defn represent [slug in-dir out-dir]
+  (let [file           (str (str/replace slug "-" "_") ".clj")
+        _              (io/copy (io/file in-dir file)
+                                (io/file "src" file))
+        _ (reset! placeholder 0)
+        _ (reset! mappings {})
+        representation (map e/emit-hygienic-form
+                            (ana.jvm/analyze-ns slug))]
+    (spit (str out-dir "mapping.json")
+          (json/generate-string (into {} (map (fn [[k v]] [v k]) @mappings))
+                                {:pretty true}))
+    (spit (str out-dir "representation.txt")
+          (with-out-str (pp/pprint representation)))
+    representation))
+
+(comment
+  (represent "two-fer" "resources/twofers/0" "resources/")
+  )
