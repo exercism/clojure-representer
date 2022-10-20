3990b37d8282635729e7d261f4c6ded4d6fd4b0f
Bobby Towers <porkostomus@mail.com>
2022-10-19 19:19:11 -0700

    [test] generate expected results for 500 twofers

--- test/clojure_representer_test.clj
+++ test/clojure_representer_test.clj
@@ -1,7 +1,28 @@
-(ns exercism.clojure-representer-test
+(ns clojure-representer-test
   (:require [clojure.test :refer :all]
-            [exercism.clojure-representer :refer :all]))
+            [clojure-representer :refer [represent]]
+            [clojure.java.io :as io])
+  (:import [java.nio.file Files Path]
+            [java.net URI]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn- as-path
+  ^Path [path]
+  (if (instance? Path path) path
+      (if (instance? URI path)
+        (java.nio.file.Paths/get ^URI path)
+        (.toPath (io/file path)))))
+
+(deftest represent-test
+  (testing "500 twofers"
+    (doseq [twofer (range 500)]
+      (is (= (represent "two-fer" "resources/twofers/0/" "resources/") 1)))))
+
+(doseq [n (range 500)]
+  (represent "two-fer" (str "resources/twofers/" n "/") (str "resources/twofers/" n "/")))
+
+(doseq [n (range 374 500)]
+  (Files/delete (as-path (io/file (str "resources/twofers/" n "/") "expected.txt"))))
+
+(io/file (str "resources/twofers/"  "/") "expected.txt")
+
+(run-tests)
