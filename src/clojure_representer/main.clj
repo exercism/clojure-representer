3274efb81a0735c474fe81c457539f7867607e44
Bobby Towers <porkostomus@mail.com>
2022-12-04 16:06:38 -0800

    append vars to mapping

--- src/clojure_representer/main.clj
+++ src/clojure_representer/main.clj
@@ -29,15 +29,14 @@
   "Returns non-nil if the code represented by `z`
    contains an unreplaced top-level def."
   [z]
-  (z/find-next-depth-first z
-                           #(and (= 'def (z/sexpr %))
+  (z/find-next-depth-first 
+   z
+   #(and (= 'def (z/sexpr %))
            ;; can't take a substring if not enough chars,
            ;; so just short-circuit bc we already know
-                                 (or (< (count (str (z/sexpr (z/right %)))) 12)
-                                     (not= "PLACEHOLDER-"
-                                           (subs (str (z/sexpr (z/right %))) 0 12))))))
-
-
+         (or (< (count (str (z/sexpr (z/right %)))) 12)
+             (not= "PLACEHOLDER-"
+                   (subs (str (z/sexpr (z/right %))) 0 12))))))
 
 (defn replace-def
   "Takes a zipper representing normalized code,
@@ -48,21 +47,26 @@
    Outputs the zipper as-is."
   [z]
   (if-not (unreplaced-def? z) z
-          (let [var (-> z (z/find-next-depth-first
-                           #(and (= 'def (z/sexpr %))
-                                 (or (< (count (str (z/sexpr (z/right %)))) 12)
-                                     (not= "PLACEHOLDER-"
-                                           (subs (str (z/sexpr (z/right %))) 0 12)))))
+          (let [var (-> z 
+                        (z/find-next-depth-first
+                         #(and (= 'def (z/sexpr %))
+                               (or (< (count (str (z/sexpr (z/right %)))) 12)
+                                   (not= "PLACEHOLDER-"
+                                         (subs (str (z/sexpr (z/right %))) 
+                                               0 12)))))
                         z/right
                         z/sexpr)
                 z2 
-                    (z/prewalk z (fn select [zloc]
-                                   (= var (z/sexpr zloc)))
-                               (fn visit [zloc]
-                                 (z/replace zloc (symbol (str "PLACEHOLDER-" @placeholder)))))]
-            
+                (z/prewalk 
+                 z (fn select [zloc]
+                     (= var (z/sexpr zloc)))
+                 (fn visit [zloc]
+                   (z/replace 
+                    zloc 
+                    (symbol (str "PLACEHOLDER-" @placeholder)))))]
               (reset! code (z/of-string (-> z2 z/root-string)))
-              (swap! mappings assoc (str (z/sexpr var)) (str "PLACEHOLDER-" @placeholder))
+              (swap! mappings assoc (str var)
+                     (str "PLACEHOLDER-" @placeholder))
               (swap! placeholder inc)
               z2)))
 
@@ -101,3 +105,26 @@
   
 (defn -main [slug in-dir out-dir]
   (represent {:slug slug :in-dir in-dir :out-dir out-dir}))
+
+(defn strip-ns [s]
+  (str/replace s "(do\n (do\n  (clojure.core/in-ns 'armstrong-numbers)\n  ((fn*\n    PLACEHOLDER-0\n    ([]\n     (do\n      (clojure.lang.Var/pushThreadBindings\n       #:clojure.lang.Compiler{LOADER\n                               (.getClassLoader\n                                (.getClass PLACEHOLDER-0))})\n      (try\n       (clojure.core/refer 'clojure.core)\n       (finally (clojure.lang.Var/popThreadBindings)))))))\n  (if\n   (.equals 'armstrong-numbers 'clojure.core)\n   nil\n   (do\n    (clojure.lang.LockingTransaction/runInTransaction\n     (fn*\n      ([]\n       (clojure.core/commute\n        @#'clojure.core/*loaded-libs*\n        clojure.core/conj\n        'armstrong-numbers))))\n    nil)))\n " ""))
+
+(re-seq #"PLACEHOLDER-\d+" 
+        (slurp (str "resources/armstrong_numbers/" 1 
+                    "/src/expected-representation.txt")))
+
+(represent {:slug    "armstrong-numbers"
+            :in-dir  (str "resources/armstrong_numbers/" 1
+                          "/src/")
+            :out-dir (str "resources/armstrong_numbers/" 1
+                          "/src/")})
+
+(slurp (str "resources/armstrong_numbers/" 1
+            "/src/armstrong_numbers.clj"))
+
+(slurp (str "resources/armstrong_numbers/" 1 
+            "/src/mapping.json"))
+
+(map #(strip-ns (slurp (str "resources/armstrong_numbers/" % 
+                            "/src/expected-representation.txt")))
+     (range 500))
